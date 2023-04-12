0d3e9fe95bcd0d41d85823cf88e48f656321d329
Siddharth Hota <80689111+Bluetooth-stack@users.noreply.github.com>
2023-04-12 15:48:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function nonRepeat(str) {
+	for(let i = 0; i<str.length-1; i++){
+		let c = str[i];
+		if(str.indexOf(c)===i){
+			if(str.indexOf(c, i+1)===-1){
+				return c;
+			}
+		}
+	}
+	return ''
+}
+let str = prompt("Enter a string");
+let res = nonRepeat(str);
+// console.log(res);
+alert(res)
